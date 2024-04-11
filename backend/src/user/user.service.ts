3fbd27108c50c89ed9906ec504c7300f51773fb9
9oodam <godamcity98@gmail.com>
2024-04-12 02:49:24 +0900

    feature: user add: entity, repository, service, controller, module

---
+++ backend/src/user/user.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@nestjs/common';
+import { UserRepository } from './user.repo';
+import { User } from './user.entity';
+
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UserService {
+  constructor(
+    private readonly userRepository: UserRepository) {}
+
+    async findAllUsers(): Promise<User[]> {
+      return this.userRepository.findAll();
+    }
+
+    async registerUser(user_id: string, user_pw: string, nickname: string): Promise<string | null> {
+      // 아이디 중복 확인
+      const user = await this.userRepository.findOneByUserId(user_id);
+      if(user) return 'duplicated user_id';
+
+      const hash = bcrypt.hashSync(user_pw, 10); // 패스워드 10자리 해시화
+      try {
+        await this.userRepository.create({
+          user_id: user_id,
+          user_pw: hash,
+          nickname: nickname
+        });
+        console.log('user table insert succeed');
+        return 'success';
+      } catch (error) {
+        console.log(error);
+        return 'failed';
+      }
+    }
+
+    async loginUser(user_id: string, user_pw: string): Promise<User | string | null> {
+      // 맞게 입력했는지 확인
+      const user = await this.userRepository.findOneByUserId(user_id);
+      if(!user) {
+        return 'wrong user_id';
+      }else {
+        const same = bcrypt.compareSync(user_pw, user.user_pw); // 입력한 패스워드와 해시값 비교
+        if(same) {
+          return user; // 맞으면 유저 정보 반환
+        }
+      }
+    }
+}
