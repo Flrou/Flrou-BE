6c230ca20651dad7b19004e2139e5307ad47e1ea
9oodam <godamcity98@gmail.com>
2024-04-15 16:34:23 +0900

    feat: gpt add: refineText 함수 추가

---
+++ backend/src/chat/chat.entity.ts
@@ -0,0 +1,19 @@
+import { AutoIncrement, BelongsTo, Column, DataType, HasMany, Model, PrimaryKey, Table } from "sequelize-typescript";
+import { User } from "src/user/user.entity";
+
+@Table({timestamps : true})
+export class Chat extends Model {
+    @PrimaryKey
+    @AutoIncrement
+    @Column
+    id: number;
+
+    @Column({type: DataType.STRING(300), allowNull: false})
+    content: string;
+
+    @Column({type: DataType.BOOLEAN})
+    isUser: boolean;
+
+    @BelongsTo(() => User, {foreignKey : 'user_id'})
+    user: User;
+}
