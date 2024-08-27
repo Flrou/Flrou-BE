2795e4418babb2c9525186e4a9f6b0e5dadada04
9oodam <godamcity98@gmail.com>
2024-08-27 18:55:51 +0900

    test3

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,12 +3,19 @@ import { AppModule } from './app.module';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 // CORS 설정
+// const corsOptions: CorsOptions = {
+//   origin: ['https://flrou.site', '192.168.0.28'], // 허용할 도메인
+//   // origin: ['http://localhost:5500'], // 허용할 도메인
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'], // 허용할 HTTP 메서드
+//   allowedHeaders: ['Content-Type', 'Authorization'], // 허용할 헤더
+//   credentials: true,
+// };
+
 const corsOptions: CorsOptions = {
-  origin: ['https://flrou.site', '192.168.0.28'], // 허용할 도메인
-  // origin: ['http://localhost:5500'], // 허용할 도메인
+  origin: 'https://flrou.site', // 허용할 도메인
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // 허용할 HTTP 메서드
   allowedHeaders: ['Content-Type', 'Authorization'], // 허용할 헤더
-  credentials: true,
+  credentials: true, // 자격 증명 허용
 };
 
 async function bootstrap() {
