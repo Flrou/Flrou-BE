dfc80f6d4a92863869613f229524bdd291d566ba
9oodam <godamcity98@gmail.com>
2024-09-23 22:46:43 +0900

    setForce chk

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -12,74 +12,95 @@ export class UserService {
     private readonly calendarService: CalendarService,
   ) {}
 
-    async findAllUsers(): Promise<User[]> {
-      return this.userRepository.findAll();
+  async findAllUsers(): Promise<User[]> {
+    return this.userRepository.findAll();
+  }
+
+  async registerUser(
+    user_id: string,
+    user_pw: string,
+    nickname: string,
+  ): Promise<string | null> {
+    // 아이디 중복 확인
+    const user = await this.userRepository.findOneByUserId(user_id);
+    if (user) return 'duplicated user_id';
+
+    const hash = bcrypt.hashSync(user_pw, 10); // 패스워드 10자리 해시화
+
+    // 아이디, 패스워드 모두 존재하면 카카오 로그인인 것으로 추정 -> 로그인 진행
+    if (hash) {
+      this.loginUser(user_id, user_pw);
     }
 
-    async registerUser(user_id: string, user_pw: string, nickname: string): Promise<string | null> {
-      // 아이디 중복 확인
-      const user = await this.userRepository.findOneByUserId(user_id);
-      if(user) return 'duplicated user_id';
+    // 유저 생성
+    try {
+      await this.userRepository.create({
+        user_id: user_id,
+        user_pw: hash,
+        nickname: nickname,
+      });
+      console.log('user table insert succeed');
+      return 'success';
+    } catch (error) {
+      console.log(error);
+      return 'failed';
+    }
+  }
 
-      const hash = bcrypt.hashSync(user_pw, 10); // 패스워드 10자리 해시화
-
-      // 아이디, 패스워드 모두 존재하면 카카오 로그인인 것으로 추정 -> 로그인 진행
-      if(hash) {
-        this.loginUser(user_id, user_pw);
+  async loginUser(
+    user_id: string,
+    user_pw: string,
+  ): Promise<User | string | null> {
+    // 맞게 입력했는지 확인
+    const user = await this.userRepository.findOneByUserId(user_id);
+    if (!user) {
+      return 'wrong user_id';
+    } else {
+      const same = bcrypt.compareSync(user_pw, user.user_pw); // 입력한 패스워드와 해시값 비교
+      if (same) {
+        return user; // 맞으면 유저 정보 반환
       }
+    }
+  }
 
-      // 유저 생성
-      try {
-        await this.userRepository.create({
-          user_id: user_id,
-          user_pw: hash,
-          nickname: nickname
-        });
-        console.log('user table insert succeed');
+  async setForce(
+    user_id: string,
+    cur_year: number,
+    cur_month: number,
+    cur_day: number,
+    alarm: number,
+  ): Promise<any> {
+    console.log('들어왔니?', user_id);
+    try {
+      // force 설정 (true/false)
+      const setting = await this.userRepository.setForce(user_id);
+      console.log('바뀐 세팅: ', setting);
+      // 강제 알림
+      if (setting) {
+        // force == true
+        await this.calendarService.setForceAlarm(
+          user_id,
+          cur_year,
+          cur_month,
+          cur_day,
+          alarm,
+        );
         return 'success';
-      } catch (error) {
-        console.log(error);
-        return 'failed';
+      } else {
+        // force == false
+        console.log('force X');
       }
+    } catch (error) {
+      return 'failed';
     }
+  }
 
-    async loginUser(user_id: string, user_pw: string): Promise<User | string | null> {
-      // 맞게 입력했는지 확인
-      const user = await this.userRepository.findOneByUserId(user_id);
-      if(!user) {
-        return 'wrong user_id';
-      }else {
-        const same = bcrypt.compareSync(user_pw, user.user_pw); // 입력한 패스워드와 해시값 비교
-        if(same) {
-          return user; // 맞으면 유저 정보 반환
-        }
-      }
+  async setDeviceToken(user_id: string, token: string): Promise<any> {
+    try {
+      const res = await this.userRepository.setDeviceToken(user_id, token);
+      return 'success';
+    } catch (error) {
+      return error;
     }
-
-    async setForce(user_id: string, cur_year: number, cur_month: number, cur_day: number, alarm: number): Promise<any> {
-      console.log('들어왔니?', user_id)
-      try {
-        // force 설정 (true/false)
-        const setting = await this.userRepository.setForce(user_id);
-        console.log('바뀐 세팅: ', setting)
-        // 강제 알림
-        if(setting) { // force == true
-          await this.calendarService.setForceAlarm(user_id, cur_year, cur_month, cur_day, alarm);
-        }else { // force == false
-          
-        }
-        return 'success';
-      } catch (error) {
-        return 'failed'
-      }
-    }
-
-    async setDeviceToken(user_id: string, token: string): Promise<any> {
-      try {
-        const res = await this.userRepository.setDeviceToken(user_id, token);
-        return 'success';
-      } catch (error) {
-        return error;
-      }
-    }
+  }
 }
